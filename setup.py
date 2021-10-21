266c33f2740cec1b44b7fa4d135a87b780f3bba1
BinaryWiz <ultimi450@gmail.com>
2021-10-20 22:30:12 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+   name='character-bert',
+   version='1.0',
+   description='CharacterBERT: Reconciling ELMo and BERT for Word-Level Open-Vocabulary Representations From Characters',
+   license="Apache License 2.0",
+   long_description=long_description,
+   author='Hicham El Boukkouri',
+   author_email='helboukkouri.dev@gmail.com',
+   url="https://github.com/Mascerade/character-bert",
+   packages=['character_bert'],
+   install_requires=['torch',
+                     'numpy',
+                     'sklearn'
+                     'transformers']
+)
